9a5d779dba0a01aa45d9ff4ce375ae7633d86a44
p.gsd <pgsd@disroot.org>
2022-11-07 18:01:14 +0100

    added the lieu search engine

---
+++ build/engines/search/lieu.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.weight = exports.request = void 0;
+const parser_1 = require("../../parser");
+async function request(query) {
+    let value = await (0, parser_1.parseResultList)('https://lieu.cblgh.org/?q=' + query, {
+        // lieu search
+        session: true,
+        resultItemPath: 'li.entry',
+        titlePath: 'a',
+        hrefPath: 'a[href]',
+        contentPath: '.TRY',
+        //suggestionPath: 'a.result-group-layout__queryCorrectedText-2Uw3R[href]',
+    });
+    console.log('returning' + value.results);
+    return value;
+}
+exports.request = request;
+exports.weight = 2;
--- src/engines/search/lieu.ts
+++ src/engines/search/lieu.ts
@@ -2,12 +2,12 @@ import { parseResultList } from '../../parser'
 import { EngineResponse } from '../../search'
 
 export async function request(query): Promise<EngineResponse> {
-	let value =  await parseResultList('https://hndex.org/?q='+query, {
-        // hndex search
+	let value =  await parseResultList('https://lieu.cblgh.org/?q='+query, {
+        // lieu search
 		session: true,
 
-		resultItemPath: 'section',
-		titlePath: 'p > cite',
+		resultItemPath: 'li.entry',
+		titlePath: 'a',
 		hrefPath: 'a[href]',
 		contentPath: '.TRY',
 		//suggestionPath: 'a.result-group-layout__queryCorrectedText-2Uw3R[href]',
